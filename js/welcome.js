c6b27d1fa547f82bc750714bcbd9e70fed49faf1
taher-shoker <taher-elshoker@hotmail.com>
2019-10-07 17:43:25 +0200

    udate code

--- js/welcome.js
+++ js/welcome.js
@@ -3,14 +3,13 @@
 /****initialize main screen****/
 
 
-/* $(document).ready(function(e) {
+ $(document).ready(function(e) {
    
 	
-	
-	$(".screen").click(function(e) {
-		$(".screen").fadeOut(2000,function(){
-			window.location.replace("main.html");
-		});
-	});
+$('.screen').on('tap', function(e) { 
+  $(this).fadeOut(2000,function(){
+    window.location.replace("main.html");
+  });
+});
 
-});/*****end doc ready***/ 
+}); 
